d82a4e8c8ee4accb795436b825499420854a8120
Madhu Yelubandy <madhuyelubandi@gmail.com>
2024-12-23 16:50:52 +0530

    axios setup

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Body = () => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Navbar />
-      <Outlet />
-    </>
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
